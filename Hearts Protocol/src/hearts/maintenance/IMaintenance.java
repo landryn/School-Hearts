372c115d155ad8b3245b03ed161fd73498106760
Michał Charmas <michal@charmas.pl>
2010-04-28 23:02:36 +0200

    dodany interfejs do realizacji protokolu nie sluzacego do gry

---
+++ Hearts Protocol/src/hearts/maintenance/IMaintenance.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hearts.maintenance;
+
+import java.io.Serializable;
+
+/**
+ * Interfejs do zadań związanych z logowaniem, rejestracją, tworzeniem stołu
+ * i innych zadań specjalnych tego typu. 
+ */
+public interface IMaintenance extends Serializable {
+    
+}
