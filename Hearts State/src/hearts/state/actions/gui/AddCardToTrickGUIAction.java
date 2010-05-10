bfc6ed743408386c8e3fefaefc54847102539bcd
Pawel <pawel@pawel-laptop.(none)>
2010-05-10 21:07:42 +0200

    Dalsze poprawki 	new file:   Hearts State/src/hearts/state/actions/gui/AddCardToTrickGUIAction.java 	new file:   Hearts State/src/hearts/state/actions/gui/AuctionEndGUIAction.java 	new file:   Hearts State/src/hearts/state/actions/gui/AuctionGUIAction.java 	new file:   Hearts State/src/hearts/state/actions/gui/ChosseTrumpGUIAction.java 	new file:   Hearts State/src/hearts/state/actions/gui/GameEndGUIAction.java 	new file:   Hearts State/src/hearts/state/actions/gui/NewDealForUserGUIAction.java 	new file:   Hearts State/src/hearts/state/actions/gui/NextTripGUIAction.java

---
+++ Hearts State/src/hearts/state/actions/gui/AddCardToTrickGUIAction.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hearts.state.actions.gui;
+
+import hearts.defs.actions.gui.AGUIAction;
+import hearts.defs.state.GUIStateException;
+import hearts.defs.state.ICard;
+import hearts.defs.state.IGUIState;
+
+/**
+ * Klasa dodaje kartę na stół. 
+ * @author Paweł Trynkiewicz
+ */
+public class AddCardToTrickGUIAction extends AGUIAction {
+
+    public AddCardToTrickGUIAction(int receiver) {
+        super(receiver);
+    }
+    protected ICard card=null;
+
+    public ICard getCard() {
+        return card;
+    }
+
+    public void setCard(ICard card) {
+        this.card = card;
+    }
+
+
+    @Override
+    public void perform(IGUIState gui) throws GUIStateException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ Hearts State/src/hearts/state/actions/gui/AuctionGUIAction.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hearts.state.actions.gui;
+
+import hearts.defs.actions.gui.AGUIAction;
+import hearts.defs.state.GUIStateException;
+import hearts.defs.state.IGUIState;
+
+/**
+ * Klasa informująca graczy licytacji. Pierwszy tę akcje otrzyna następny gracz po
+ * wychodzącym graczu z iformacją o maksymalnej ofercie w tym przypadku 0. W odpowiedzi odsyła AuctionOfferAction,
+ * z jego ofertą. Aukcja kończy się w momęcie gdy, w licytacji zostanie jeden gracz.
+ * @author Paweł Trynkiewicz
+ */
+public class AuctionGUIAction extends AGUIAction{
+
+    int lider;
+    int commece;
+    int quotion;
+    int activeUser;
+
+    public int getCommece() {
+        return commece;
+    }
+
+    public void setCommece(int commece) {
+        this.commece = commece;
+    }
+
+    public int getLider() {
+        return lider;
+    }
+
+    public void setLider(int lider) {
+        this.lider = lider;
+    }
+
+    public int getQuotion() {
+        return quotion;
+    }
+
+    public void setQuotion(int quotion) {
+        this.quotion = quotion;
+    }
+
+    public int getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(int activeUser) {
+        this.activeUser = activeUser;
+    }
+    
+
+    
+    
+
+    public AuctionGUIAction(int receiver) {
+        super(receiver);
+    }
+
+    @Override
+    public void perform(IGUIState gui) throws GUIStateException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+
+
+}
